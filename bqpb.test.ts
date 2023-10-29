b97cc4d51132ffb6f0c06ea493b3716ecb2fcd66
Masaki Hara <ackie.h.gmai@gmail.com>
2023-10-29 17:56:25 +0900

    Implement wire parser

--- bqpb.test.ts
+++ bqpb.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
-import { decodeBase64 } from "./bqpb.ts";
+import { decodeBase64, parseWire } from "./bqpb.ts";
 
 Deno.test("decodeBase64", async (t) => {
   const table: [string, number[]][] = [
@@ -38,4 +38,89 @@ Deno.test("decodeBase64", async (t) => {
       assertEquals(actual, Uint8Array.from(expected));
     });
   }
+});
+
+Deno.test("parseWire", async (t) => {
+  await t.step("parses empty bytes", () => {
+    const actual = parseWire(Uint8Array.from([]));
+    assertEquals(actual, []);
+  });
+  await t.step("parses multiple fields", () => {
+    const actual = parseWire(Uint8Array.from([112, 50, 88, 54]));
+    assertEquals(actual, [
+      {
+        f: 14n,
+        w: 0,
+        v: 50n,
+      },
+      {
+        f: 11n,
+        w: 0,
+        v: 54n,
+      },
+    ]);
+  });
+  await t.step("parses VARINT", () => {
+    const actual = parseWire(Uint8Array.from([240, 35, 221, 144, 7]));
+    assertEquals(actual, [
+      {
+        f: 574n,
+        w: 0,
+        v: 116829n,
+      },
+    ]);
+  });
+  await t.step("parses I64", () => {
+    const actual = parseWire(
+      Uint8Array.from([49, 17, 18, 19, 20, 21, 22, 23, 24]),
+    );
+    assertEquals(actual, [
+      {
+        f: 6n,
+        w: 1,
+        v: 0x1817161514131211n,
+      },
+    ]);
+  });
+  await t.step("parses I32", () => {
+    const actual = parseWire(Uint8Array.from([53, 17, 18, 19, 20]));
+    assertEquals(actual, [
+      {
+        f: 6n,
+        w: 5,
+        v: 0x14131211n,
+      },
+    ]);
+  });
+  await t.step("parses LEN", () => {
+    const actual = parseWire(Uint8Array.from([66, 5, 1, 2, 3, 4, 5]));
+    assertEquals(actual, [
+      {
+        f: 8n,
+        w: 2,
+        v: Uint8Array.from([1, 2, 3, 4, 5]),
+      },
+    ]);
+  });
+  await t.step("parses GROUP", () => {
+    const actual = parseWire(Uint8Array.from([75, 112, 50, 88, 54, 76]));
+    assertEquals(actual, [
+      {
+        f: 9n,
+        w: 3,
+        v: [
+          {
+            f: 14n,
+            w: 0,
+            v: 50n,
+          },
+          {
+            f: 11n,
+            w: 0,
+            v: 54n,
+          },
+        ],
+      },
+    ]);
+  });
 });
--- bqpb.ts
+++ bqpb.ts
@@ -43,6 +43,177 @@ export function decodeBase64(b64: string): Uint8Array {
   return arr;
 }
 
-// @ts-expect-error TODO
-export function parse(base64Input: string): object {
+type WireState = {
+  /** buffer */
+  readonly b: Uint8Array;
+  /** position */
+  p: number;
+};
+
+function readByte(state: WireState): number {
+  if (state.p >= state.b.length) throw new Error("Unexpected EOF");
+  return state.b[state.p++];
+}
+
+function readVarint(state: WireState): bigint {
+  let result = 0n;
+  let shift = 0n;
+  while (true) {
+    const current = readByte(state);
+    result |= BigInt(current & 127) << shift;
+    if (current < 128) break;
+    shift += 7n;
+  }
+  return result;
+}
+
+export type WireField = IntegralWireField | BytesWireField | GroupWireField;
+export type IntegralWireField = {
+  /** field number */
+  f: bigint;
+  /** wire type */
+  w: 0 | 1 | 5;
+  /** value */
+  v: bigint;
+};
+export type BytesWireField = {
+  /** field number */
+  f: bigint;
+  /** wire type */
+  w: 2;
+  /** value */
+  v: Uint8Array;
+};
+export type GroupWireField = {
+  /** field number */
+  f: bigint;
+  /** wire type */
+  w: 3;
+  /** value */
+  v: WireField[];
+};
+
+function readFields(state: WireState, endGroup?: bigint): WireField[] {
+  const fields: WireField[] = [];
+  while (true) {
+    const field = readField(state, endGroup);
+    if (field == null) break;
+    fields.push(field);
+  }
+  return fields;
+}
+
+function readField(state: WireState, endGroup?: bigint): WireField | null {
+  if (endGroup == null && state.p >= state.b.length) return null;
+
+  const tag = readVarint(state);
+  const wireType = Number(tag & 7n);
+  const fieldNumber = tag >> 3n;
+  switch (wireType) {
+    // VARINT
+    case 0: {
+      const value = readVarint(state);
+      return { f: fieldNumber, w: wireType, v: value };
+    }
+    // I64 / I32
+    case 1:
+    case 5: {
+      const len = wireType === 1 ? 8 : 4;
+      let value = 0n;
+      for (let i = 0; i < len; i++) {
+        const byte = readByte(state);
+        value |= BigInt(byte) << BigInt(i * 8);
+      }
+      return { f: fieldNumber, w: wireType, v: value };
+    }
+    // LEN
+    case 2: {
+      const length = Number(readVarint(state));
+      if (state.p + length > state.b.length) throw new Error("Unexpected EOF");
+      const value = state.b.subarray(state.p, state.p + length);
+      state.p += length;
+      return { f: fieldNumber, w: wireType, v: value };
+    }
+    // SGROUP
+    case 3: {
+      const fields = readFields(state, fieldNumber);
+      return { f: fieldNumber, w: wireType, v: fields };
+    }
+    // EGROUP
+    case 4: {
+      if (fieldNumber === endGroup) return null;
+      throw new Error("Invalid group");
+    }
+    default:
+      throw new Error("Unexpected wire type");
+  }
+}
+
+export function parseWire(input: Uint8Array): WireField[] {
+  const state = { b: input, p: 0 };
+  return readFields(state);
+}
+
+function inferScalarType(field: IntegralWireField): string {
+  const { w: wireType, v: value } = field;
+
+  // Infer floating point numbers
+  if (wireType === 1) {
+    const fabs = value & 0x7FFFFFFFFFFFFFFFn;
+    if (
+      // zero
+      fabs === 0n ||
+      // reasonable range of normalized numbers
+      0x3000000000000000n <= fabs && fabs < 0x5000000000000000n ||
+      // infinity
+      fabs === 0x7FF0000000000000n ||
+      // popular NaN representation
+      fabs === 0x7FF8000000000000n
+    ) {
+      return "double";
+    }
+  }
+  if (wireType === 5) {
+    const fabs = value & 0x7FFFFFFFn;
+    if (
+      // zero
+      fabs === 0n ||
+      // reasonable range of normalized numbers
+      0x30000000n <= fabs && fabs < 0x50000000n ||
+      // infinity
+      fabs === 0x7F800000n ||
+      // popular NaN representation
+      fabs === 0x7FC00000n
+    ) {
+      return "float";
+    }
+  }
+
+  // Infer signed integers
+  if (0xE0000000n <= value && value < 0x100000000n) {
+    if (wireType === 0) return "int32";
+    if (wireType === 5) return "fixed32";
+  }
+  if (0xE000000000000000n <= value && value < 0x10000000000000000n) {
+    if (wireType === 0) return "int64";
+    if (wireType === 1) return "fixed64";
+  }
+
+  // Fall back to unsigned integers
+  if (wireType === 1) return "fixed64";
+  if (wireType === 5) return "fixed32";
+  return "uint64";
+
+  // Not inferred:
+  // - bool
+  // - enum
+  // - signed-succinct variants (sint32, sint64, sfixed32, sfixed64)
+}
+
+export function parseBytes(input: Uint8Array): object {
+  throw new Error("TODO");
+}
+
+export function parse(input: string): object {
+  return parseBytes(decodeBase64(input));
 }
